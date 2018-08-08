56629e92fd249a56124a2342182c3c32321eda35
James Houston <houstonj1@wit.edu>
2018-08-08 14:52:55 -0400

    Refactor for assert function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,22 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("James")
-		want := "Hello, James"
-
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("James")
+		want := "Hello, James"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
