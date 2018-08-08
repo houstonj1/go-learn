2203dfb20c04739a88db230fd6021c7e2ab21c28
James Houston <houstonj1@wit.edu>
2018-08-08 14:50:34 -0400

    Added another test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("James")
-	want := "Hello, James"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("James")
+		want := "Hello, James"
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
 
 }
