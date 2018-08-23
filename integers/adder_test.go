754edab063c6042f1b4953b5ddc116c13c28e665
James Houston <houstonj1@wit.edu>
2018-08-22 20:11:10 -0400

    Added example for Add function

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,7 +1,11 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
+// Test Add() function
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
@@ -9,4 +13,11 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+// Example Add() function
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Expected output: 6
 }
