01d7e90837e6fdfc45f70d1e0ee83822f4d79a62
James Houston <houstonj1@wit.edu>
2018-08-23 22:19:58 -0400

    Added better test comparison

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -21,7 +24,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
