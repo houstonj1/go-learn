f8865d71b5bf3c4537ade709709dbe6ff52fbd46
James Houston <houstonj1@wit.edu>
2018-09-21 10:10:48 -0400

    Implemented area function

--- strucs_methods_interfaces/shapes.go
+++ strucs_methods_interfaces/shapes.go
@@ -4,3 +4,8 @@ package strucsmethodsinterfaces
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// Area function
+func Area(width float64, height float64) float64 {
+	return width * height
+}
