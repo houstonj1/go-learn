c3ff0624cf1fa9d63287e5b4d682549e5d8fde9a
James Houston <houstonj1@wit.edu>
2018-12-30 21:26:08 -0500

    Added wallet struct with tests for Balance and Deposit methods

---
+++ pointers_and_errors/wallet.go
@@ -0,0 +1,16 @@
+package errors
+
+// Wallet struct
+type Wallet struct {
+	balance int
+}
+
+// Deposit method
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+// Balance method
+func (w Wallet) Balance() int {
+	return w.balance
+}
