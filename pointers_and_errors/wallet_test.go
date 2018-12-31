3cd9639d9734fae61b371941307a317cf4b489d6
James Houston <houstonj1@wit.edu>
2018-12-30 23:32:35 -0500

    Refactored tests, added test to ensure no error on successful Withdraw

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -6,26 +6,6 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	assertBalance := func(t *testing.T, wallet Wallet, want Dollar) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("didn't get an error but wanted one")
-		}
-
-		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Dollar(10))
@@ -34,8 +14,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Dollar(20)}
-		wallet.Withdraw(Dollar(10))
+		err := wallet.Withdraw(Dollar(10))
 		assertBalance(t, wallet, Dollar(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -45,4 +26,31 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Dollar(20))
 		assertError(t, err, ErrInsufficientFunds)
 	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Dollar) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
 }
