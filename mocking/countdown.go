8c80fea4dfc746b0689b27616aacd711318962d4
James Houston <james-houston@pluralsight.com>
2019-11-30 05:50:46 -0500

    feat(mocking) Added sleeper and spySleeper to countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,8 +10,21 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+// Sleeper type
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper struct
+type DefaultSleeper struct{}
+
+// Sleep function
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Countdown function
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
@@ -22,5 +35,6 @@ func Countdown(out io.Writer) {
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
