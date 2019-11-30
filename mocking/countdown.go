dde082d1ea226ecb3989e6c359cec6c7be8ddcff
James Houston <james-houston@pluralsight.com>
2019-11-30 05:33:23 -0500

    chore(mocking) Passing tests and refactor

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 // Countdown function
 func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
